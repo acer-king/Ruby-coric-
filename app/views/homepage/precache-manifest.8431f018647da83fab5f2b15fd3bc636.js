4703d3d2950cedaa4a05c7534434fefff73043bc
acer-king <acerking03@gmail.com>
2020-12-06 05:55:25 -0800

    init state

---
+++ app/views/homepage/precache-manifest.8431f018647da83fab5f2b15fd3bc636.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69ad5838ddf52ca9a10fb61eb851b1df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f9cd599dac2eb88fdaf",
+    "url": "/static/css/2.f477f4b5.chunk.css"
+  },
+  {
+    "revision": "85c9e427a5375edf4e41",
+    "url": "/static/css/main.9e04234f.chunk.css"
+  },
+  {
+    "revision": "6f9cd599dac2eb88fdaf",
+    "url": "/static/js/2.832d6870.chunk.js"
+  },
+  {
+    "revision": "4839ef9924f06b3bc63565baf9fa3db4",
+    "url": "/static/js/2.832d6870.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85c9e427a5375edf4e41",
+    "url": "/static/js/main.26cd2b3e.chunk.js"
+  },
+  {
+    "revision": "92f55db03cb30ea55e2d",
+    "url": "/static/js/runtime-main.e28ef27b.js"
+  },
+  {
+    "revision": "6f26998885a98ead5b9773deeb0833da",
+    "url": "/static/media/card_type.6f269988.jpg"
+  },
+  {
+    "revision": "ed98c157bfad107db3692885434fc0d9",
+    "url": "/static/media/patient.ed98c157.jpg"
+  },
+  {
+    "revision": "4788048f319dc48101678d9e69f5077e",
+    "url": "/static/media/profile-placeholder.4788048f.png"
+  },
+  {
+    "revision": "00990ea472c4b6083acc9b906151d3ed",
+    "url": "/static/media/therapist.00990ea4.jpg"
+  },
+  {
+    "revision": "65d6940ec117e823481686699ef1fddc",
+    "url": "/static/media/therapists-grid_2x.65d6940e.png"
+  },
+  {
+    "revision": "9299706f23346da2776f4aef1e52996a",
+    "url": "/static/media/wave.9299706f.png"
+  }
+]);
